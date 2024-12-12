86f7b1f1f7e91ff239ec00d1958cd482f1ca9133
Pedro Bernardo <pedro_bernardo@icloud.com>
2024-12-12 14:32:53 +0100

    re-add assertions testharness.js

--- resources/testharness.js
+++ resources/testharness.js
@@ -4590,7 +4590,7 @@
      */
     function assert(expected_true, function_name, description, error, substitutions)
     {
-        if (expected_true !== true && false) {
+        if (expected_true !== true){ // && false) {
             var msg = make_message(function_name, description,
                                    error, substitutions);
             throw new AssertionError(msg);
